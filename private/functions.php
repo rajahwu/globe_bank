2e62a10e389bf716ae54b65679fde8570651f4cf
rajahwu <vmradford@gmail.com>
2023-08-21 00:14:08 -0400

    create encoding funcitons

--- private/functions.php
+++ private/functions.php
@@ -8,4 +8,14 @@ function url_for($script_path) {
     return WWW_ROOT . $script_path;
 }
 
+function u($string="") {
+    return urldecode($string);
+}
+function raw_u($string="") {
+    return rawurldecode($string);
+}
+
+function h($string="") {
+    return htmlspecialchars($string);
+}
 ?>
