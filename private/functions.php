4a91415aa262488c263a785fd3d60fbdc6c1aed9
rajahwu <vmradford@gmail.com>
2023-08-21 09:50:05 -0400

    add create new subject and edit subject forms

--- private/functions.php
+++ private/functions.php
@@ -1,32 +1,44 @@
-<?php 
+<?php
 
-function url_for($script_path) {
+function url_for($script_path)
+{
     // add the leading '/' if not present
-    if($script_path[0] != '/') {
+    if ($script_path[0] != '/') {
         $script_path = "/" . $script_path;
     }
     return WWW_ROOT . $script_path;
 }
 
-function u($string="") {
+function u($string = "")
+{
     return urldecode($string);
 }
-function raw_u($string="") {
+function raw_u($string = "")
+{
     return rawurldecode($string);
 }
 
-function h($string="") {
+function h($string = "")
+{
     return htmlspecialchars($string);
 }
 
-function error_404() {
+function error_404()
+{
     header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
     exit();
 }
 
-function error_500() {
+function error_500()
+{
     header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
     exit();
-    
+
+}
+
+function redirect_to($location)
+{
+    header("Location: " . $location);
+    exit();
 }
 ?>
