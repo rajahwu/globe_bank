9219af54692b9aac5b31f30aa30d6de85194b8e8
rajahwu <vmradford@gmail.com>
2023-08-20 21:33:53 -0400

    create staff_header and staff_footer components

--- public/staff/index.php
+++ public/staff/index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../stylesheets/staff.css" />
-    <title>GBI</title>
-</head>
-
-<body>
-    <header>
-        <h1>GBI Staff Area</h1>
-    </header>
-    <navigation>
-        <ul>
-            <li><a href="" index.php">Menu</a></li>
-        </ul>
-    </navigation>
-    <div id="content"></div>
-    <footer>
-        &copy;
-        <?php echo date('Y'); ?> Globe Bank
-    </footer>
-</body>
-
-</html>
+<?php require_once('../../private/initialize.php'); ?>
+<?php include('../../private/shared/staff_header.php'); ?>
+<div id="content"></div>
+<?php include('../../private/shared/staff_footer.php'); ?>
