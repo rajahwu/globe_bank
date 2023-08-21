f9dba0b36e731a8c1b09b7204f6abde2c4a44805
rajahwu <vmradford@gmail.com>
2023-08-21 11:22:46 -0400

    add html encoding to create page and edit page and subject forms

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -1,29 +1,27 @@
-<?php 
+<?php
 
-require_once('../../../private/initialize.php'); 
+require_once('../../../private/initialize.php');
 
-?>
 
-<?php 
-if(!isset($_GET['id'])) {
-    redirect_to('/staff/pages/index.php');
+if (!isset($_GET['id'])) {
+  redirect_to('/staff/pages/index.php');
 }
 $id = $_GET['id'];
 
 $menu_name = '';
-$postion ='';
+$postion = '';
 $visible = '';
 
-if(is_post_request()) {
-    $menu_name = $_POST["menu_name"];
-    $position = $_POST["position"];
-    $visible = $_POST["visible"];
+if (is_post_request()) {
+  $menu_name = $_POST["menu_name"];
+  $position = $_POST["position"];
+  $visible = $_POST["visible"];
 
-    
-    echo "Form parameters<br />";
-    echo "Menu name: " . $menu_name . "<br />";
-    echo "Position: " . $position . "<br />";
-    echo "Visible: " . $visible . "<br />";
+
+  echo "Form parameters<br />";
+  echo "Menu name: " . $menu_name . "<br />";
+  echo "Position: " . $position . "<br />";
+  echo "Visible: " . $visible . "<br />";
 }
 
 ?>
@@ -36,18 +34,20 @@ if(is_post_request()) {
   <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
 
   <div class="page new">
-    <h1>Create Page</h1>
+    <h1>Edit Page</h1>
 
     <form action="<?php echo url_for('/staff/pages/edit.php?id=') . h(u($id)); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?php echo $menu_name ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name) ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+            <option value="1" <?php if ($position == "1") {
+              echo " selected";
+            } ?>>1</option>
           </select>
         </dd>
       </dl>
@@ -55,11 +55,13 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" />
+          <input type="checkbox" name="visible" value="1" <?php if ($visible == "1") {
+            echo " checked";
+          } ?> />
         </dd>
       </dl>
       <div id="operations">
-        <input type="submit" value="Create Subject" />
+        <input type="submit" value="Edit Page" />
       </div>
     </form>
 
--- public/staff/pages/new.php
+++ public/staff/pages/new.php
@@ -1,23 +1,20 @@
-<?php 
+<?php
 
-require_once('../../../private/initialize.php'); 
+require_once('../../../private/initialize.php');
 
-?>
-
-<?php 
 $menu_name = '';
-$position ='';
+$position = '';
 $visible = '';
 
-if(is_post_request()) {
-    $menu_name = $_POST["menu_name"];
-    $position = $_POST["position"];
-    $visible = $_POST["visible"];
+if (is_post_request()) {
+  $menu_name = $_POST["menu_name"];
+  $position = $_POST["position"];
+  $visible = $_POST["visible"];
 
-    echo "Form parameters<br />";
-    echo "Menu name: " . $menu_name . "<br />";
-    echo "Position: " . $position . "<br />";
-    echo "Visible: " . $visible . "<br />";
+  echo "Form parameters<br />";
+  echo "Menu name: " . $menu_name . "<br />";
+  echo "Position: " . $position . "<br />";
+  echo "Visible: " . $visible . "<br />";
 }
 
 ?>
@@ -35,13 +32,15 @@ if(is_post_request()) {
     <form action="<?php echo url_for('/staff/pages/new.php'); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?php echo $menu_name ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name) ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+            <option value="1" <?php if ($position == "1") {
+              echo " selected";
+            } ?>>1</option>
           </select>
         </dd>
       </dl>
@@ -49,7 +48,9 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" />
+          <input type="checkbox" name="visible" value="1" <?php if ($visible == "1") {
+            echo " checked";
+          } ?> />
         </dd>
       </dl>
       <div id="operations">
