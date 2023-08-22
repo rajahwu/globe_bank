db69207c53688acdb484bc83ebe2d8313726b109
rajahwu <vmradford@gmail.com>
2023-08-21 22:01:23 -0400

    get form options from db

--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 
-require_once('../../../private/initialize.php'); 
+require_once('../../../private/initialize.php');
 
-if(!isset($_GET['id'])) {
+if (!isset($_GET['id'])) {
   redirect_to(url_for('/staff/subjects/index.php'));
 }
 
 $id = $_GET['id'];
 
-if(is_post_request()) {
+if (is_post_request()) {
   // Handle form values sent by new.php
   $subject = [];
   $subject['id'] = $id;
-  $subject['menu_name' ]= $_POST['menu_name'] ?? '';
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
   $subject['position'] = $_POST['position'] ?? '';
   $subject['visible'] = $_POST['visible'] ?? '';
 
@@ -20,8 +20,12 @@ if(is_post_request()) {
   redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
 
 } else {
-  
+
   $subject = find_subject_by_id($id);
+  
+  $subject_set = find_all_subjects();
+  $subject_count = mysqli_num_rows($subject_set);
+  mysqli_free_result($subject_set);
 }
 ?>
 
@@ -44,9 +48,15 @@ if(is_post_request()) {
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1"<?php if ($subject['position'] == "1") {
-              echo " selected";
-            } ?>>1</option>
+            <?php
+            for ($i = 1; $i <= $subject_count; $i++) {
+              echo "<option value=\"{$i}\"";
+              if ($subject["position"] == $i) {
+                echo " selected";
+              }
+              echo ">{$i}</option>";
+            }
+            ?>
           </select>
         </dd>
       </dl>
@@ -54,7 +64,7 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1"<?php if ($subject['visible'] == "1") {
+          <input type="checkbox" name="visible" value="1" <?php if ($subject['visible'] == "1") {
             echo " checked";
           } ?> />
         </dd>
